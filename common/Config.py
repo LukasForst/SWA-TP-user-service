895ba10b245dedfcf506067bf1868536e54bce5d
Lukas Forst <lukas@forst.pw>
2020-04-27 16:02:31 +0200

    add ignored stuf

---
+++ common/Config.py
@@ -0,0 +1,57 @@
+import logging
+import os
+from dataclasses import dataclass
+from typing import Optional
+
+from flask import current_app as app
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class Config:
+    postgres_user: str
+    postgres_password: str
+    postgres_db: str
+    postgres_url: str
+
+
+def get_config() -> Config:
+    """
+    Obtains configuration from the application context.
+    """
+    return get_or_set('config', build_configuration)
+
+
+def build_configuration() -> Config:
+    """
+    Builds configuration from environment or from the Flask properties
+    """
+    logger.debug('Building configuration.')
+    config = Config(postgres_user=get_prop('POSTGRES_USER', True),
+                    postgres_password=get_prop('POSTGRES_PASSWORD', True),
+                    postgres_db=get_prop('POSTGRES_DB', True),
+                    postgres_url=get_prop('POSTGRES_URL', True))
+    logger.debug(f'Used configuration: {config}')
+    return config
+
+
+def get_prop(name: str, optional: bool = False) -> str:
+    """
+    Gets property from environment or from the flask env.
+    """
+    config = os.environ.get(name, app.config.get(name))
+    if not optional and not config:
+        logger.error(f'It was not possible to retrieve configuration for property "{name}"!')
+        raise EnvironmentError(f'No existing configuration for "{name}" found!')
+    return config
+
+
+def get_or_set(prop: str, factory: Callable):
+    """
+    Gets or sets context property.
+    """
+    if not hasattr(g, prop):
+        setattr(g, prop, factory())
+
+    return getattr(g, prop)
---
+++ local_config.py
@@ -0,0 +1,4 @@
+POSTGRES_USER = 'super-cool-user'
+POSTGRES_PASSWORD = 'super-secret-pwd'
+POSTGRES_DB = 'user-service'
+POSTGRES_URL = 'localhost:5432'

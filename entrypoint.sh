b74039a365c314e4fbf6caa32b661d3ad01a957e
Lukas Forst <lukas@forst.pw>
2020-05-17 21:07:40 +0200

    update entrypoint and wait on DB to start up

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
+# wait on db
 # we use automatic migration as this is dev environment
-flask db upgrade
+./wait-for-it.sh "${POSTGRES_URL}" -- flask db upgrade
+
 # startup servicce
 gunicorn --bind 0.0.0.0:8080 --log-level WARNING app:app
